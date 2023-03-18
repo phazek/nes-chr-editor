54d29878571fe2d1bb90fdc39a19e995454b0d44
Zsombor, Fazekas <fazekas.zsombor@gmail.com>
2023-03-18 02:26:52 +0100

    Reorganise editor app private section

--- src/editor/application.h
+++ src/editor/application.h
@@ -1,6 +1,7 @@
 #include "olc/olcPixelGameEngine.h"
 #include "olc/olcPGEX_QuickGUI.h"
 
+#include "model.h"
 #include "style.h"
 
 #include <unordered_map>
@@ -15,14 +16,16 @@ public:
 	bool OnUserCreate() override;
 	bool OnUserUpdate(float fElapsedTime) override;
 private:
+	olc::QuickGUI::Manager guiManager_;
+	Model editorModel_;
+	std::vector<std::unique_ptr<olc::QuickGUI::BaseControl>> controls_;
+	std::unordered_map<olc::QuickGUI::BaseControl*, std::function<void()>> controlHandlers_;
+	Style style_;
+
 	bool HandleInput();
 	void DrawScene();
 	void BuildUI();
 
-	Style style_;
-	olc::QuickGUI::Manager guiManager_;
-	std::vector<std::unique_ptr<olc::QuickGUI::BaseControl>> controls_;
-	std::unordered_map<olc::QuickGUI::BaseControl*, std::function<void()>> controlHandlers_;
 };
 
 } // namespace Editor
