e8aebfd1c7848728557ee67a1926d7eb49e65dba
Zsombor, Fazekas <fazekas.zsombor@gmail.com>
2023-03-22 12:42:33 +0100

    Fix indentation

--- src/editor/ui/uielements.h
+++ src/editor/ui/uielements.h
@@ -2,25 +2,24 @@
 
 namespace Editor::UI {
 
-	class ColorButton : public olc::QuickGUI::Button
-	{
-	public:
-		ColorButton(olc::QuickGUI::Manager& manager,
-			olc::Pixel color,
-			const std::string& text,
-			const olc::vf2d& pos,
-			const olc::vf2d& size);
+class ColorButton : public olc::QuickGUI::Button
+{
+public:
+	ColorButton(olc::QuickGUI::Manager& manager,
+		olc::Pixel color,
+		const std::string& text,
+		const olc::vf2d& pos,
+		const olc::vf2d& size);
 
-		void SetColor(olc::Pixel color);
-		void SetText(const std::string& text);
+	void SetColor(olc::Pixel color);
+	void SetText(const std::string& text);
 
-		void Draw(olc::PixelGameEngine* pge) override;
-		void DrawDecal(olc::PixelGameEngine* pge) override;
-	private:
-		olc::Pixel color_;
-		olc::Sprite sprite_;
-		olc::Sprite spritePressed_;
-
-	};
+	void Draw(olc::PixelGameEngine* pge) override;
+	void DrawDecal(olc::PixelGameEngine* pge) override;
+private:
+	olc::Pixel color_;
+	olc::Sprite sprite_;
+	olc::Sprite spritePressed_;
+};
 
 } //namespace Editor::UI
