794528931b849461b7feb394a5ebc70fbde947e9
Zsombor, Fazekas <fazekas.zsombor@gmail.com>
2023-03-22 14:24:36 +0100

    Return a control handle after creating ui element

--- src/editor/ui/uimanager.cpp
+++ src/editor/ui/uimanager.cpp
@@ -4,6 +4,12 @@
 
 namespace Editor::UI {
 
+void Manager::ControlHandle::SetVisibility(bool visible) {
+	for (auto* control : controls) {
+		control->bVisible = visible;
+	}
+}
+
 Manager::Manager(olc::PixelGameEngine& engine, const Style& style)
 : engine_(engine) {
 	guiManager_.colText = style.textColor;
@@ -27,25 +33,47 @@ void Manager::Draw() {
 	}
 }
 
-void Manager::AddLabel(const std::string& text, const olc::vf2d& pos, const olc::vf2d& size) {
-	new olc::QuickGUI::Label(guiManager_, text, pos, size);
+Manager::ControlHandle Manager::AddLabel(const std::string& text, const olc::vf2d& pos, const olc::vf2d& size) {
+	Manager::ControlHandle ret;
+	ret.controls.push_back(new olc::QuickGUI::Label(guiManager_, text, pos, size));
+	return ret;
 }
 
-void Manager::AddColorButton(const olc::Pixel& color, const std::string& text, const olc::vf2d& pos,
-	const olc::vf2d& size, UpdateHandler_t handler) {
+Manager::ControlHandle Manager::AddColorButton(const olc::Pixel& color,
+					       const std::string& text,
+					       const olc::vf2d& pos,
+					       const olc::vf2d& size,
+					       UpdateHandler_t handler) {
+	Manager::ControlHandle ret;
+
 	auto* button = new ColorButton(guiManager_, color, text, pos, size);
 	updateHandlers_[button] = handler;
+
+	ret.controls.push_back(button);
+	return ret;
 }
 
-void Manager::AddButtonStrip(uint8_t buttonCount, const olc::vf2d& pos, const olc::vf2d& size, std::function<void(int)> buttonHandler) {
+Manager::ControlHandle Manager::AddButtonStrip(
+    uint8_t buttonCount, const olc::vf2d& pos, const olc::vf2d& size,
+    std::function<void(int)> buttonHandler) {
+	Manager::ControlHandle ret;
+
 	auto buttonWidth = std::min(static_cast<int>(size.x) / buttonCount, 30);
 	auto leftPad = (size.x - buttonWidth * buttonCount) / 2;
 	for (int i = 0; i < buttonCount; ++i) {
-		auto* button = new olc::QuickGUI::Button(guiManager_, std::to_string(i), {pos.x + leftPad + buttonWidth * i, pos.y}, {static_cast<float>(buttonWidth), 30});
-		updateHandlers_[button] = [buttonHandler, i](olc::QuickGUI::BaseControl*) {
+	    auto* button = new olc::QuickGUI::Button(
+			guiManager_, std::to_string(i),
+			{pos.x + leftPad + buttonWidth * i, pos.y},
+			{static_cast<float>(buttonWidth), 30});
+
+	    updateHandlers_[button] = [buttonHandler, i](olc::QuickGUI::BaseControl*) {
 			buttonHandler(i);
-		};
+	    };
+
+		ret.controls.push_back(button);
 	}
+
+	return ret;
 }
 
 } // namespace Editor::UI
--- src/editor/ui/uimanager.h
+++ src/editor/ui/uimanager.h
@@ -9,15 +9,25 @@ namespace Editor::UI {
 class Manager {
 public:
 	using UpdateHandler_t = std::function<void(olc::QuickGUI::BaseControl*)>;
+	struct ControlHandle {
+		void SetVisibility(bool visible);
+		std::vector<olc::QuickGUI::BaseControl*> controls;
+	};
+
 
 	Manager(olc::PixelGameEngine& engine, const Style& style);
 	void Update();
 	void Draw();
 
-	void AddLabel(const std::string& text, const olc::vf2d& pos, const olc::vf2d& size);
-	void AddColorButton(const olc::Pixel& color, const std::string& text, const olc::vf2d& pos,
-		const olc::vf2d& size, UpdateHandler_t handler);
-	void AddButtonStrip(uint8_t buttonCount, const olc::vf2d& pos, const olc::vf2d& size, std::function<void(int)> buttonHandler);
+	ControlHandle AddLabel(const std::string& text, const olc::vf2d& pos, const olc::vf2d& size);
+	ControlHandle AddColorButton(const olc::Pixel& color,
+				     const std::string& text,
+				     const olc::vf2d& pos,
+				     const olc::vf2d& size,
+				     UpdateHandler_t handler);
+	ControlHandle AddButtonStrip(
+	    uint8_t buttonCount, const olc::vf2d& pos, const olc::vf2d& size,
+	    std::function<void(int)> buttonHandler);
 
 private:
 	olc::PixelGameEngine& engine_;
