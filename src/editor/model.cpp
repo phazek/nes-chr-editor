5c2ade4e6e6c76ee10d88cdde5df485426ef90ca
Zsombor, Fazekas <fazekas.zsombor@gmail.com>
2023-03-22 12:05:25 +0100

    Implement sprite atlas rendering

--- src/editor/model.cpp
+++ src/editor/model.cpp
@@ -1,8 +1,25 @@
 #include "model.h"
 
 #include "nes/constants.h"
+#include "nes/palette.h"
 
 #include "tfm/tinyformat.h"
+
+namespace {
+struct Tile {
+	std::array<uint8_t, 8 * 8> data;
+
+	void FromData(const std::span<uint8_t>& src) {
+		for (int row = 0; row < 8; ++row) {
+			for (int col = 0; col < 8; ++col) {
+				bool ll = !!(src[row] & (1 << (7 - col)));
+				bool hh = !!(src[8 + row] & (1 << (7 - col)));
+				data[row * 8 + col] = (hh ? 2 : 0) | (ll ? 1 : 0);
+			}
+		}
+	}
+};
+} // namespace
 
 namespace Editor {
 
@@ -36,7 +53,25 @@ olc::Sprite& Model::GetSpriteAtlas() {
 }
 
 void Model::UpdateSprite() {
-	// TODO
+	static Tile t;
+	std::array<olc::Pixel, 4> colors = {
+		nes::kColorPalette[palette_[0]],
+		nes::kColorPalette[palette_[1]],
+		nes::kColorPalette[palette_[2]],
+		nes::kColorPalette[palette_[3]],
+	};
+
+	for (int spriteIdx = 0; spriteIdx < 256; ++spriteIdx) {
+		std::span<uint8_t> input{data_.data() + spriteIdx * 16, 16};
+		olc::vi2d topLeft = {(spriteIdx % 16) * 8, spriteIdx / 16 * 8};
+		t.FromData(input);
+		for (int y = 0; y < 8; ++y) {
+			for (int x = 0; x < 8; ++x) {
+				auto colorId = t.data[y * 8 + x];
+				spriteAtlas_.SetPixel(topLeft.x + x, topLeft.y + y, colors[colorId]);
+			}
+		}
+	}
 }
 
 } // namespace Editor
