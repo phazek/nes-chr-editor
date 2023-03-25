4e6504766c342a0a6f9882235c51b9877966b480
Zsombor, Fazekas <fazekas.zsombor@gmail.com>
2023-03-25 19:54:03 +0100

    Add ButtonStrip implementation

--- src/editor/ui/uielements.cpp
+++ src/editor/ui/uielements.cpp
@@ -51,4 +51,67 @@ void ColorButton::SetText(const std::string& text) {
 	sText = text;
 }
 
+// ButtonStrip
+ButtonStrip::ButtonStrip(
+	olc::PixelGameEngine& pge,
+	const olc::vf2d& pos,
+	const olc::vf2d& size,
+	const Style& style)
+: engine_(pge)
+, pos_(pos)
+, size_(size)
+, style_(style) {
+	SetButtonCount(3);
+}
+
+void ButtonStrip::Update(float fElapsedTime) {
+	auto boundsCheck = [](const olc::vi2d& mousePos, const Button& button) {
+	    return (button.pos.x < mousePos.x && button.pos.y < mousePos.y) &&
+		   (button.pos.x + button.size.x > mousePos.x &&
+		    button.pos.y + button.size.y > mousePos.y);
+	};
+
+	auto mousePos = engine_.GetMousePos();
+	auto mousePressed = engine_.GetMouse(olc::Mouse::LEFT).bPressed;
+	auto mouseReleased = engine_.GetMouse(olc::Mouse::LEFT).bReleased;
+	for (int i = 0; i < buttons_.size(); ++i) {
+	    auto& button = buttons_[i];
+	    if (boundsCheck(mousePos, button)) {
+			if (button.pressed && mouseReleased) {
+				button.pressed = false;
+				buttonHandler_(i);
+			} else if (mousePressed) {
+				button.pressed = true;
+			}
+	    }
+	}
+}
+
+void Editor::UI::ButtonStrip::Draw() {
+	for (int i = 0; i < buttons_.size(); ++i) {
+		auto& button = buttons_[i];
+		auto color = button.pressed ? style_.highlightColor: style_.bgColor;
+		engine_.FillRect(button.pos, button.size, color);
+		engine_.DrawRect(button.pos, button.size, style_.borderColor);
+		auto textSize = engine_.GetTextSize(button.text);
+		olc::vf2d pos{(button.size.x - textSize.x) / 2, (button.size.y - textSize.y) / 2};
+		pos += button.pos;
+		engine_.DrawString(pos, button.text, style_.textColor);
+	}
+}
+
+void Editor::UI::ButtonStrip::SetButtonCount(unsigned int count) {
+	buttons_.clear();
+	auto btnWidth = size_.x / count;
+	for (int i = 0; i < count; ++i) {
+		buttons_.push_back({std::to_string(i+1),
+			olc::vf2d{pos_.x + i * btnWidth, pos_.y},
+			olc::vf2d{btnWidth, size_.y}});
+	}
+}
+
+void Editor::UI::ButtonStrip::SetButtonHandler(Handler_t handler) {
+	buttonHandler_ = std::move(handler);
+}
+
 } //namespace Editor::UI
--- src/editor/ui/uielements.h
+++ src/editor/ui/uielements.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "olc/olcPGEX_QuickGUI.h"
+#include "../style.h"
+#include <functional>
 
 namespace Editor::UI {
 
@@ -22,6 +24,34 @@ private:
 	olc::Pixel color_;
 	olc::Sprite sprite_;
 	olc::Sprite spritePressed_;
+};
+
+class ButtonStrip {
+public:
+	using Handler_t = std::function<void(int)>;
+
+	ButtonStrip(olc::PixelGameEngine& pge, const olc::vf2d& pos,
+		    const olc::vf2d& size, const Style& style);
+
+	void Update(float fElapsedTime);
+	void Draw();
+	void SetButtonCount(unsigned int count);
+	void SetButtonHandler(Handler_t handler);
+
+private:
+	struct Button {
+		std::string text;
+		olc::vf2d pos;
+		olc::vf2d size;
+		bool pressed = false;
+	};
+
+	olc::PixelGameEngine& engine_;
+	olc::vf2d pos_;
+	olc::vf2d size_;
+	const Style& style_;
+	Handler_t buttonHandler_;
+	std::vector<Button> buttons_;
 };
 
 } //namespace Editor::UI
