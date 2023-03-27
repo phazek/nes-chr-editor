4322e1f2f5aed77f4d00f776aec42a3dd18b246a
Zsombor, Fazekas <fazekas.zsombor@gmail.com>
2023-03-27 23:08:38 +0200

    Add app versioning

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include "editor/application.h"
 #include "argparse/argparse.hpp"
+#include "version.h"
 
 int main(int argc, char** argv) {
-	argparse::ArgumentParser program("NES CHR editor");
+	argparse::ArgumentParser program("NES CHR editor", APP_VERSION_STRING);
 
 	program.add_argument("path")
 	.required()
@@ -10,8 +11,7 @@ int main(int argc, char** argv) {
 
 	try {
 		program.parse_args(argc, argv);
-	}
-		catch (const std::runtime_error& err) {
+	} catch (const std::runtime_error& err) {
 		std::cerr << err.what() << std::endl;
 		std::cerr << program;
 		std::exit(1);
